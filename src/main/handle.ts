efa9b3ec368c5dc37f3d7365c508539bae21b0e2
chenhb23 <chenhongbo_1994@163.com>
2020-11-09 15:48:41 +0800

    主线程解析 url

--- src/main/handle.ts
+++ src/main/handle.ts
@@ -1,6 +1,6 @@
 import {BrowserWindow, DownloadItem, ipcMain, dialog, shell} from 'electron'
 import path from 'path'
-import {debounce} from '../common/util'
+import {debounce, delay} from '../common/util'
 import store from './store'
 import config from '../project.config'
 import IpcEvent from '../common/IpcEvent'
@@ -97,6 +97,46 @@ function setupDialog(win: BrowserWindow) {
   })
 }
 
+function setupParse() {
+  ipcMain.handle('parse', async (event, frame: string) => {
+    let win = new BrowserWindow({
+      show: false,
+      skipTaskbar: true,
+      frame: false,
+    })
+
+    function listenUrl() {
+      return new Promise(resolve => {
+        win.webContents.session.webRequest.onResponseStarted(async details => {
+          if (details.url.endsWith('/ajaxm.php')) {
+            const href = await checkHref()
+            resolve(href)
+          }
+        })
+      })
+    }
+
+    function checkHref(): Promise<string> {
+      return win.webContents
+        .executeJavaScript(
+          `;(() => {var a = document.querySelector('#go a');return a ? a.getAttribute('href') : null})()`
+        )
+        .then(value => {
+          if (value) return value
+          else return delay(300).then(checkHref)
+        })
+    }
+
+    await win.loadURL(frame)
+    const href = await listenUrl()
+    delay(500).then(() => {
+      win.destroy()
+      win = null
+    })
+    return href
+  })
+}
+
 let initial = false
 export function unsetup() {
   ipcMain.removeHandler(IpcEvent.trigger)
@@ -116,6 +156,7 @@ export function setup(win: BrowserWindow) {
   setupDownload(win)
   setupStore(win)
   setupDialog(win)
+  setupParse()
 
   initial = true
 }
